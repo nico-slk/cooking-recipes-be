14834ff484a279a594eeefe33eb303ac7dcd0fac
nico-slk <nicolas.selicki@gmail.com>
2025-01-17 15:32:32 -0300

    log

--- src/db/db.ts
+++ src/db/db.ts
@@ -6,9 +6,10 @@ const { mysqlport, mysqldatabase, mysqlpassword, mysqluser } = Config;
 const dbURL = `mysql://${mysqluser}:${mysqlpassword}@viaduct.proxy.rlwy.net:35853/${mysqldatabase}`;
 
 const db = new Sequelize(dbURL, {
+  logging: console.log,
   dialect: "mysql",
   dialectModule: mysql2,
-  logging: false,
+  // logging: false,
   port: +mysqlport,
   timezone: "-03:00",
 });
